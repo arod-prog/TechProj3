6d9ed1278cf5bec39e4da123ca4f10ab873c7d5a
arod-prog <54718639+arod-prog@users.noreply.github.com>
2021-09-20 16:25:50 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace TechProj3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask user expected grade
+            Console.WriteLine("Kilometer Increment Value");
+
+
+            {
+                string input = Console.ReadLine();
+
+                decimal speed = decimal.Parse(input);
+                double KM = 0.621371;
+
+                if (10 <= speed && speed <= 25)
+                {
+                    Console.WriteLine("Kilometer" + " " + "Equiv Miles");
+                    Console.WriteLine("0" + "          " + speed);
+
+                    decimal ispeed = speed + 10;
+                    while (ispeed < 201)
+                    {
+                        Console.WriteLine(ispeed + "         " + (ispeed * (decimal)KM));
+                        ispeed +=10;
+
+
+
+                    }
+
+
+                }
+                else
+                {
+                   // this stall and results in nothing when the disered input is not entered
+                }
+            }    
+        }
+    }
+}
+
